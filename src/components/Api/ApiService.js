d2e56e438c84c9042513237c75af243cf492baff
Islam-Baytiev <topmuradov@gmail.com>
2022-06-12 13:56:04 +0300

    надо сделать error

--- src/components/Api/ApiService.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-
-const  ApiService = async () => {
-  const url = `https://api.themoviedb.org/3/search/movie?api_key=4a4472922ec446302808be760563492d&query=return&page=1`
-  const res = await fetch(url);
-  return res.json()
-};
-export default ApiService
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,50 +1,58 @@
 import React, { useEffect, useState} from "react";
 import './App.css'
-import ApiService from "../Api/ApiService";
 import Card from "../Card/Card";
 import Spinner from "../Spinner/Spinner";
+import axios from "axios";
+import { Row, Col, Input } from 'antd';
 import Error from "../Error/Error";
+
 
 function App()  {
 
-  const [movies,setMovies] = useState();
+  const [movies,setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(true)
   const [isError, setIsError] = useState(false)
 
-  function onError(err) {
-    console.log(123)
-    setIsError(true)
-    setIsLoading(false)
-  }
-
-  const getResource =  () => {
-    return ApiService().then(res => {
-        setMovies(res)
-        setIsLoading(false)
-    }).catch(onError)
-  }
-
 
   useEffect( () => {
-    getResource()
+    const fetchMovie = async () => {
+      try {
+        const {data} = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=4a4472922ec446302808be760563492d&query=return&page=1`);
+        return data
+      }
+      catch (err) {
+        setIsError(true)
+      }
+      finally {
+        setIsLoading(false)
+      }
+    }
+    fetchMovie().then(response => setMovies(response))
+
   }, [])
 
-
   return (
-      <>
-        <section className="container">
-          <div className="movie" >
-            {isLoading === true ?
-                <Spinner isLoading={isLoading} />
-                :
-                <>{movies?.results.map(el => <Card load = {isLoading} {...el}/>)}</>
-            }
-          </div>
-        </section>
-      </>
+      <Row>
+        <Col span={16} offset={4}
+             style={{paddingTop: 80, paddingBottom: 36}}>
+          {isLoading === true ?
+              <Spinner isLoading={isLoading} />
+              :
+              <>{movies?.results.map(el => <Card load = {isLoading} {...el}/>)}</>
+          }
+        </Col>
+      </Row>
+
   );
 }
 
 export default App
 
 
+/*      <>
+        <section className="container">
+          <div className="movie" >
+
+          </div>
+        </section>
+      </>*/
